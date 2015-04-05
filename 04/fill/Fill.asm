d658311b57682f650fe0515cb62809775e8afd4d
diskkid <disk2id@gmail.com>
2015-04-06 01:13:48 +0900

    Implement Fill.asm

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -9,3 +9,62 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+(KBD_LOOP)
+  @R0
+  M=0
+  @KBD
+  D=M
+  @BLACK_LOOP
+  D;JGT
+  @WHITE_LOOP
+  D;JEQ
+  @KBD_LOOP
+  0;JMP
+
+(WHITE_LOOP)
+  @SCREEN
+  D=A
+  @R0
+  D=D+M
+  A=D
+  M=0
+  @R0
+  M=M+1
+  D=M
+  @8192
+  D=D-A
+  @WHITE_LOOP
+  D;JLE
+(WHITE_KBD)
+  @R0
+  M=0
+  @KBD
+  D=M
+  @BLACK_LOOP
+  D;JGT
+  @WHITE_KBD
+  0;JMP
+
+(BLACK_LOOP)
+  @SCREEN
+  D=A
+  @R0
+  D=D+M
+  A=D
+  M=-1
+  @R0
+  M=M+1
+  D=M
+  @8192
+  D=D-A
+  @BLACK_LOOP
+  D;JLE
+(BLACK_KBD)
+  @R0
+  M=0
+  @KBD
+  D=M
+  @WHITE_LOOP
+  D;JEQ
+  @BLACK_KBD
+  0;JMP
