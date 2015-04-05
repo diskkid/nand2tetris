e31ce8bb1606e83764061791cf2a5e935bc7113e
diskkid <disk2id@gmail.com>
2015-04-06 00:12:21 +0900

    Implement Mult.asm

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -7,3 +7,30 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+// Initialize R2 (result register)
+  @R2
+  M=0
+// If R0 = 0 or R1 = 0, jump to END
+  @R0
+  D=M
+  @END
+  D;JLE
+  @R1
+  D=M
+  @END
+  D;JLE
+(LOOP)
+  @R0
+  D=M
+  @R2
+  M=D+M
+  @R1
+  M=M-1
+  D=M
+  @END
+  D;JEQ
+  @LOOP
+  0;JMP
+(END)
+  @END
+  0;JMP
