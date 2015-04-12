11c5e06db5acede06c05606d33247786ffb651dd
diskkid <disk2id@gmail.com>
2015-04-12 16:35:25 +0900

    Remove disused files.

--- 07/ParserTest.rb
+++
@@ -1,8 +0,0 @@
-require './Parser.rb'
-
-parser = Parser.new(File.new('./ParserTestCode.txt'))
-
-while(parser.hasMoreCommands)
-  puts "#{parser.commandType} #{parser.arg1} #{parser.arg2}"
-  parser.advance
-end
